ffbc17df0eb2cecdb39f495a7df92fdcf7fc551f
qinbeihai <qinwei.hfut@gmail.com>
2023-06-19 18:41:52 +0800

    add argpare module; split the data into training data and dev data

--- train/data_prepro/convert_for_atomhub.py
+++ train/data_prepro/convert_for_atomhub.py
@@ -1,5 +1,7 @@
 ﻿import pandas as pd
 import json
+import random
+import argparse
 
 def convert_to_formatted_list(file_path):
     with open(file_path, "r") as f:
@@ -33,9 +35,32 @@ def convert_list_to_csv(dialogues, csv_file_path):
     df = pd.DataFrame(dialogues)
     df.to_csv(csv_file_path, index=False)
 
-if __name__ == "__main__":
-    file_path = "./financial_xiaozhu/xiaozhu.json"
-    csv_saved_path = "./financial_xiaozhu/xiaozhu_saved.csv"
-    dialogues = convert_to_formatted_list(file_path)
-    convert_list_to_csv(dialogues, csv_saved_path)
+def split_data(num_dev, dialogues):
+    random.shuffle(dialogues)  # 随机打乱列表顺序
+    dialogues_dev = dialogues[:num_dev]  # 前num_dev个作为dev集
+    dialogues_train = dialogues[num_dev:]  # 剩下的作为train集
+    return dialogues_train, dialogues_dev
 
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Convert JSON to CSV")
+    parser.add_argument("--file_path", type=str, default="./financial_xiaozhu/xiaozhu.json",
+                        help="Path to the input JSON file")
+    parser.add_argument("--training_data_path", type=str, 
+                        help="Path to save the training data")
+    parser.add_argument("--dev_data_path", type=str,
+                        help="Path to save the dev data")
+    parser.add_argument("--num_dev", type=int, default=30,
+                        help="The number of dev set")
+    args = parser.parse_args()
+    file_path = args.file_path
+    training_data_path = args.training_data_path
+    dev_data_path = args.dev_data_path
+    num_dev = args.num_dev
+    dialogues = convert_to_formatted_list(file_path)
+    dialogues_train, dialogues_dev = split_data(num_dev, dialogues)
+    convert_list_to_csv(dialogues_train, training_data_path)
+    convert_list_to_csv(dialogues_dev, dev_data_path)
+
+
+# python3 ./train/data_prepro/convert_for_atomhub.py --file_path ./financial_xiaozhu/xiaozhu.json --training_data_path ./financial_xiaozhu/training.csv --dev_data_path ./financial_xiaozhu/test.csv --num_dev 100
